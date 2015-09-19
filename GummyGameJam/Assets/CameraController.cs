5a497cdccdcda3a8fe9454c7b6c23b962b8e73f2
unknown <johnss18@SA2502-PC03.win.rpi.edu>
2015-09-19 17:14:14 -0400

    movement works a-okay, with jump and a little rolling and theres a camera and stuff

---
+++ GummyGameJam/Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform player1;
+	public List<Transform> entityList;
+	// Use this for initialization
+	void Start () {
+		entityList.Add(player1);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 focalPoint = new Vector2();
+		foreach(Transform t in entityList)
+		{
+			focalPoint += new Vector2(t.position.x, t.position.y);
+		}
+		//now normalize that vector by the number of entities
+		focalPoint /= entityList.Count;
+		//now lerp there
+		Vector3 lerpSpot = new Vector3(focalPoint.x, focalPoint.y, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position,lerpSpot , .1f);
+	}
+}
--- GummyGameJam/Assets/PlayerController.cs
+++ GummyGameJam/Assets/PlayerController.cs
@@ -5,22 +5,46 @@ public class PlayerController : MonoBehaviour
 {
 	public int playerNum;
 	public int state;	//0 moving, 1 left stick, 2 right stick, 3 both stick
+	public bool stuck = false;
+	
+
+	public Rigidbody2D rb;
+	public float maxSpeed;
+	public float moveForce;
+	public float jumpForce;
+	public bool onGround;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		rb = GetComponent<Rigidbody2D>();
 	}
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		UpdateOnGround();
+		if(!stuck)
+			MovementEngine();
 	}
-	void ProcessInput()
+	void MovementEngine()
 	{
-		if(Input.GetKeyDown(KeyCode.A))
+		//rb.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, );
+		if(Mathf.Abs(rb.velocity.x) < Mathf.Abs(maxSpeed))
+			rb.AddForce(new Vector2(Input.GetAxis("Horizontal")* moveForce,0), ForceMode2D.Force);
+		else
 		{
-
+			rb.AddTorque(-1 * Mathf.Sign(rb.velocity.x));	//make gummy rotate
+			rb.velocity = new Vector3(maxSpeed * Mathf.Sign(rb.velocity.x), rb.velocity.y);	//cap the speed
 		}
+		if(onGround && Input.GetButtonDown("Jump"))
+		{
+			rb.AddForce(new Vector2(0f,jumpForce), ForceMode2D.Impulse);
+		}
+	}
+	void UpdateOnGround()
+	{
+		if(Physics2D.Raycast(transform.position, Vector2.down, .55f))
+		   onGround = true;
+		else onGround = false;
 	}
 }
