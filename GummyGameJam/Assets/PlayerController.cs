e53c1689ee0352c37ab3a217ce5f11bd21faf501
unknown <johnss18@SA2502-PC03.win.rpi.edu>
2015-09-19 16:05:34 -0400

    started playercontroller

---
+++ GummyGameJam/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public int playerNum;
+	public int state;	//0 moving, 1 left stick, 2 right stick, 3 both stick
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void ProcessInput()
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+
+		}
+	}
+}
